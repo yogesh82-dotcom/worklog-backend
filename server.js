b75c5f90a226762c0c41067cb541d4c27eee21ce
yogesh82-dotcom <81764342+yogesh82-dotcom@users.noreply.github.com>
2024-11-19 23:28:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin: https://yogesh82-dotcom.github.io/worklog/ }))
+app.use(cors({origin: "https://yogesh82-dotcom.github.io/worklog/" }))
 
 
 let logs = [];
